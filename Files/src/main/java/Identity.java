8e7caad104763014e0b4351ce82566aa62d3cb83
Sherajus Salehin <sherajussalehin@gmail.com>
2025-12-03 22:23:34 +0600

    until assignment 1

---
+++ Files/src/main/java/Identity.java
@@ -0,0 +1,62 @@
+import java.time.LocalDate;
+public class Identity {
+
+    String name, course, purpose, greetings;
+    String date= String.valueOf(LocalDate.now());
+    String day=date.substring(8);
+    int month=Integer.parseInt(date.substring(5,7));
+    String Month=getMonth(month);
+    String year=date.substring(0,4);
+    public Identity() {
+        name = "Sherajus Salehin";
+        course="Spring Boot Development";
+        purpose="To learn about industry standard and scalable projects,\nwith focus on E-commerce backend systems.";
+        greetings="Hello everyone!";
+    }
+
+    String AllInfo(){
+        return "name: "+name+"\ncourse name:"+course+"\nPurpose: "+purpose+"\nThe current date today: "+Month+" "+day+", "+year+"\n"+greetings;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public String getPurpose() {
+        return purpose;
+    }
+    String getMonth(int m){
+        String month="";
+        switch (m){
+            case 1:
+                month="JAN";
+            case 2:
+                month="Feb";
+            case 3:
+                month="Mar";
+            case 4:
+                month="Apr";
+            case 5:
+                month="May";
+            case 6:
+                month="Jun";
+            case 7:
+                month="Jul";
+            case 8:
+                month="Aug";
+            case 9:
+                month="Sep";
+            case 10:
+                month="Oct";
+            case 11:
+                month="Nov";
+            case 12:
+                month="Dec";
+        }
+
+        return month;
+    }
+
+}
---
+++ Files/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Identity i1= new Identity();
+        System.out.println(i1.AllInfo());
+        System.out.println("\n### Module Summary ###");
+        System.out.println("1.Java basics, JDK, variables"+"\n2.Project setup");
+        System.out.println("what I learned: Refreshed the basics of JAVA."+"\nProject created: 1(assignment purpose). \nSuggestion: No suggestion till now.");
+    }
+}
