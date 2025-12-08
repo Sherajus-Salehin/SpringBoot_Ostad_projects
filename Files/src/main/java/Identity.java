ec6ee2248242ee7374a650d223b43fcd44597fdd
Sherajus Salehin <sherajussalehin@gmail.com>
2025-12-08 22:11:12 +0600

    fixed switch case

--- Files/src/main/java/Identity.java
+++ Files/src/main/java/Identity.java
@@ -30,30 +30,18 @@ public class Identity {
     String getMonth(int m){
         String month="";
         switch (m){
-            case 1:
-                month="JAN";
-            case 2:
-                month="Feb";
-            case 3:
-                month="Mar";
-            case 4:
-                month="Apr";
-            case 5:
-                month="May";
-            case 6:
-                month="Jun";
-            case 7:
-                month="Jul";
-            case 8:
-                month="Aug";
-            case 9:
-                month="Sep";
-            case 10:
-                month="Oct";
-            case 11:
-                month="Nov";
-            case 12:
-                month="Dec";
+            case 1 ->month="JAN";
+            case 2-> month="Feb";
+            case 3-> month="Mar";
+            case 4-> month="Apr";
+            case 5-> month="May";
+            case 6-> month="Jun";
+            case 7-> month="Jul";
+            case 8-> month="Aug";
+            case 9-> month="Sep";
+            case 10-> month="Oct";
+            case 11-> month="Nov";
+            case 12-> month="Dec";
         }
 
         return month;
