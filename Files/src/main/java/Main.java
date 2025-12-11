52ae57eb080dfe10d08562557bba7974b128a632
Sherajus Salehin <sherajussalehin@gmail.com>
2025-12-11 22:47:17 +0600

    fixed switch case

--- Files/src/main/java/Main.java
+++ Files/src/main/java/Main.java
@@ -1,10 +1,57 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        Sales saleCalculation= new Sales();
+        //sales info collection
+        System.out.print("Enter selling price of potato per kg: ");
+        double potatoPrice=s.nextDouble();
+        System.out.print("Enter selling price  onion per kg: ");
+        double onionPrice=s.nextDouble();
+        System.out.print("Enter selling price of oil per liter: ");
+        double OilPrice=s.nextDouble();
+        System.out.print("Total amount of potato sold: ");
+        double potatoSold=s.nextDouble();
+        System.out.print("Total amount of  onion sold: ");
+        double onionSold=s.nextDouble();
+        System.out.print("Total amount of  oil sold: ");
+        double OilSold=s.nextDouble();
+        saleCalculation.setPrices(potatoPrice,onionPrice,OilPrice);
+        saleCalculation.setSales(potatoSold,onionSold,OilSold);
+        //Cost info collection (re-using previous variables) here, sold=purchased
+        System.out.print("Enter Purchasing price of potato per kg: ");
+        potatoPrice=s.nextDouble();
+        System.out.print("Enter Purchasing price  onion per kg: ");
+        onionPrice=s.nextDouble();
+        System.out.print("Enter Purchasing price of oil per liter: ");
+        OilPrice=s.nextDouble();
+        System.out.print("Total amount of potato purchased: ");
+        potatoSold=s.nextDouble();
+        System.out.print("Total amount of  onion purchased: ");
+        onionSold=s.nextDouble();
+        System.out.print("Total amount of  oil purchased: ");
+        OilSold=s.nextDouble();
+        System.out.print("Enter total Transport cost: ");
+        int transportCost=s.nextInt();
+        saleCalculation.setPurchasePrice(potatoPrice,onionPrice,OilPrice,transportCost);
+        saleCalculation.setPurchased(potatoSold,onionSold,OilSold);//sold=purchased
+
+
+        System.out.println(saleCalculation.saleSummary()+"\n"+saleCalculation.CostSummary()
+        +"\n"+saleCalculation.netIncome());
+
+        s.close();
+
+
+
+
+        /*  ASSIGNMENT 1
         Identity i1= new Identity();
         System.out.println(i1.AllInfo());
         System.out.println("\n### Module Summary ###");
         System.out.println("1.Java basics, JDK, variables"+"\n2.Project setup");
         System.out.println("what I learned: Refreshed the basics of JAVA."+"\nProject created: 1(assignment purpose). \nSuggestion: No suggestion till now.");
-    }
+    */
+         }
 }
---
+++ Files/src/main/java/Sales.java
@@ -0,0 +1,64 @@
+public class Sales {
+    double potatoPrice, OnionPrice,OilPrice, potatoSold,OnionSold,OilSold;
+    double potatoRevenue,onionRevenue, oilRevenue, totalRvenue;
+    double potatoPurchasePrice, onionPurchasePrice,oilPurchasePrice, potatoPurchased, onionPurchased, oilPurchased;
+    double potatoCost,onionCost, oilCost, totalCost;
+    int TransportCost;
+
+    void setPrices(double potato, double onion,double oil){
+        potatoPrice=potato;
+        OnionPrice=onion;
+        OilPrice=oil;
+    }
+    void setSales(double potato, double onion,double oil){
+        potatoSold=potato;
+        OnionSold=onion;
+        OilSold=oil;
+    }
+
+    void setPurchasePrice(double potato,double onion, double oil, int transport){
+        potatoPurchasePrice=potato;
+        onionPurchasePrice=onion;
+        oilPurchasePrice=oil;
+        TransportCost=transport;
+    }
+    void setPurchased(double potato, double onion,double oil){
+        potatoPurchased=potato;
+        onionPurchased=onion;
+        oilPurchased=oil;
+    }
+
+    void calculateRevenue(){
+        potatoRevenue= potatoPrice*potatoSold;
+        onionRevenue=OnionPrice*OnionSold;
+        oilRevenue= OilPrice*OilSold;
+        totalRvenue=potatoRevenue+onionRevenue+oilRevenue;
+    }
+    void calculateCost(){
+        potatoCost= potatoPurchasePrice*potatoPurchased;
+        onionCost=onionPurchasePrice*onionPurchased;
+        oilCost= oilPurchasePrice*oilPurchased;
+        totalCost=potatoCost+onionCost+oilCost;
+    }
+
+    String saleSummary(){
+        calculateRevenue();
+        return "---Sales Data---"
+                +"\nPotato Revenue: ["+potatoRevenue+"]"
+                +"\nOnion Revenue: ["+onionRevenue+"]"
+                +"\nOil Revenue: ["+oilRevenue+"]"
+                +"\nTotal Sold Price: ["+totalRvenue+"]";
+    }
+    String CostSummary(){
+        calculateCost();
+        return "---Cost Data---"
+                +"\nPotato Purchase Cost: ["+potatoCost+"]"
+                +"\nOnion Purchase Cost: ["+onionCost+"]"
+                +"\nOil Purchase Cost: ["+oilCost+"]"
+                +"\nTotal Purchase Cost: ["+totalCost+"]"
+                +"\nTotal Transportation Cost: ["+TransportCost+"]";
+    }
+    String netIncome(){
+        return "--- Final Result ---\nNet Income: ["+(totalRvenue - totalCost-TransportCost)+"]";
+    }
+}
