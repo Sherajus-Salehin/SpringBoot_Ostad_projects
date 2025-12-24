abdd85a5ccc59992318ecd71cb99dab2b32f056b
Sherajus Salehin <sherajussalehin@gmail.com>
2025-12-24 17:38:35 +0600

    ASSIGNMENT 4 -> DEADLINE 27 DECEMBER

--- Files/src/main/java/Main.java
+++ Files/src/main/java/Main.java
@@ -1,8 +1,52 @@
 import java.util.Scanner;
 
 public class Main {
+    // ASSIGNMENT 4 -> DEADLINE 27 DECEMBER
+    static int[] UserInput(Scanner uI){
+        System.out.println("Enter the length in meters: ");
+        int l=uI.nextInt();
+        System.out.println("Enter the width in meters: ");
+        int w=uI.nextInt();
+        if(l*w<0){
+            System.err.println("Length and the width must be a positive Integer. ");
+            return UserInput(uI);
+        }
+        return new int[]{l,w};
+    }
+    static int calculation(boolean area, int l, int w){
+        if(area) return l*w;
+        else return 2*l+2*w;
+    }
+    static void display(int l, int w,int res, boolean c){
+        if(c) System.out.println("The area of the rectangle for given \nlength:"+l+"meters\nwidth: "+w+"meters\nis: "+res+" square meters.");
+        else System.out.println("The Perimeter of the area for given \nlength: "+l+"meters\nwidth: "+w+"meters\nis: "+res+" meters.");
+    }
+
     public static void main(String[] args) {
-      //ASSIGNMENT 3 -> DEADLINE 18 DECEMBER 2025.
+    // ASSIGNMENT 4 -> DEADLINE 27 DECEMBER
+        Scanner sc=new Scanner(System.in);
+        int [] inputs= UserInput(sc);
+        System.out.println("Enter: \n <1> for area.\n <2> for perimeter.");
+        int choice= sc.nextInt();
+        boolean area=(choice==1);
+        int result=calculation(area,inputs[0],inputs[1]);
+        display(inputs[0],inputs[1],result,area);
+        sc.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*ASSIGNMENT 3 -> DEADLINE 18 DECEMBER 2025.
+
     consoleATM ca=new consoleATM();
     Scanner sc=new Scanner(System.in);
     boolean looping=true;
@@ -40,21 +84,8 @@ public class Main {
         sc.close();
         System.out.println("Thank you for using the ATM! Goodbye,");
 
+        //ASSIGNMENT 2 -> DEADLINE 11 DECEMBER 2025
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*ASSIGNMENT 2 -> DEADLINE 11 DECEMBER 2025
         Scanner s=new Scanner(System.in);
         Sales saleCalculation= new Sales();
         //sales info collection
