30621ec1347263111adbef54f296b2e728f7b01c
sherajus_salehin <sherajussalehin@gmail.com>
2026-01-06 00:25:55 +0600

    few tests

--- Files/src/main/java/Main.java
+++ Files/src/main/java/Main.java
@@ -1,40 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    // ASSIGNMENT 4 -> DEADLINE 27 DECEMBER
-    static int[] UserInput(Scanner uI){
-        System.out.println("Enter the length in meters: ");
-        int l=uI.nextInt();
-        System.out.println("Enter the width in meters: ");
-        int w=uI.nextInt();
-        if(l*w<0){
-            System.err.println("Length and the width must be a positive Integer. ");
-            return UserInput(uI);
-        }
-        return new int[]{l,w};
-    }
-    static int calculation(boolean area, int l, int w){
-        if(area) return l*w;
-        else return 2*l+2*w;
-    }
-    static void display(int l, int w,int res, boolean c){
-        if(c) System.out.println("The area of the rectangle for given \nlength:"+l+"meters\nwidth: "+w+"meters\nis: "+res+" square meters.");
-        else System.out.println("The Perimeter of the area for given \nlength: "+l+"meters\nwidth: "+w+"meters\nis: "+res+" meters.");
-    }
-
     public static void main(String[] args) {
-    // ASSIGNMENT 4 -> DEADLINE 27 DECEMBER
-        Scanner sc=new Scanner(System.in);
-        int [] inputs= UserInput(sc);
-        System.out.println("Enter: \n <1> for area.\n <2> for perimeter.");
-        int choice= sc.nextInt();
-        boolean area=(choice==1);
-        int result=calculation(area,inputs[0],inputs[1]);
-        display(inputs[0],inputs[1],result,area);
-        sc.close();
-
-
-
+        C c= new C();
 
 
 
--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # SpringBoot_Ostad_projects
 My project files from the [course](https://ostad.app/dashboard/my-courses/68de1fcae5002c78ab39ce61) will be stored here.
 
-
-Assignment 4(27 december 2025) contents:
-1. [Main.java](https://github.com/Sherajus-Salehin/SpringBoot_Ostad_projects/tree/main/Files/src/main/java/Assignment4)
-
-
-Assignment 3(18 december 2025) contents:
-1. [Files](https://github.com/Sherajus-Salehin/SpringBoot_Ostad_projects/tree/main/Files/src/main/java/Assignment3)
