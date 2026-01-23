95b70b71fd8d4e06d0f9dbf32547031d88c18c0a
sherajus_salehin <sherajussalehin@gmail.com>
2026-01-23 16:13:34 +0600

    Assignment 8, done : C, remaining: RUD

--- Files/src/main/java/Assignment8/NoteManager.java
+++ Files/src/main/java/Assignment8/NoteManager.java
@@ -14,8 +14,9 @@ public class NoteManager {
             System.out.println("Press 1 to Create New Note\nPress 2 to View All Notes\nPress 3 to Update Note\nPress 4 to Delete Note\nPress 5 to Exit");
             Scanner sc = new Scanner(System.in);
             int choice = sc.nextInt();
+            sc.nextLine();
             switch (choice) {
-                case 1 -> System.out.println("Creating");
+                case 1 -> System.out.println(create());
                 case 2 -> System.out.println("Viewing All Notes");
                 case 3 -> System.out.println("Update Note");
                 case 4 -> System.out.println("Delete Note");
@@ -24,35 +25,39 @@ public class NoteManager {
         }
     }
 
-    static int create(Scanner sc) {
+    static int create() {
+        Scanner sc2 = new Scanner(System.in);
         System.out.println("Enter note:");
-        String content = sc.nextLine();
-        System.out.println("Enter note name");
-        String name = sc.nextLine();
-        Path p = Paths.get(name);
+        String content = sc2.nextLine();
+        Scanner sc3 = new Scanner(content);
+        System.out.println("Enter note name: ");
+        String name = sc2.nextLine();
+        name=name+".txt";
+        Path p = Paths.get("out/Assignment8 notes",name);
         if (Files.exists(p)) {
             System.out.println("Note already exists. Overwrite? y/n");
-            char c = sc.next().charAt(0);
+            char c = sc2.next().charAt(0);
             if (c == 'n' || c == 'N') {
-                return -1;
+                return create();
             }else{
                 //overwrite logic
+                return write(sc2,p,name,content);
             }
         } else {
             //write logic
+            return write(sc2,p,name,content);
         }
-        return 0;
     }
     static int write(Scanner sc, Path p,String name, String content) {
-        try {
+        try { //study files.writeString();
             Files.write(p, content.getBytes(StandardCharsets.UTF_8),
                     StandardOpenOption.CREATE,
                     StandardOpenOption.TRUNCATE_EXISTING);
             return 1;
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
+            return 0;
         }
-    return 0;
     }
 
     static String read() {
