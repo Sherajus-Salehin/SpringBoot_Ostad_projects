3aa600e2af5c951ef3b574a52ad3de692184c7fc
Sherajus Salehin <sherajussalehin@gmail.com>
2026-02-11 23:34:58 +0600

    Initialised and added endpoints: /info /goal /learning-summary /review

---
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class StudentController {
+ //map /info /goal /learning-summary /review:
+    @GetMapping("/info")
+    public String getInfo(){
+        LocalDate date=LocalDate.now();
+        String today= date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        return "Sherajus Salehin\nSpring Boot Development\n"+today+"\nHello, fellow developers!";
+    }
+    @GetMapping("/goal")
+    public String knowGoal(){
+        return "My goal is to be and survive this market as a JAVA developer";
+    }
+    @GetMapping("/learning-summary")
+    public String summary(){
+        return "virtual threading, springboot initialization, rest-api basics";
+    }
+    @GetMapping("/review")
+    public String review(){
+        return "Projects created: 1\nProject class was fine, could use more planning for more efficient class.";
+    }
+}
