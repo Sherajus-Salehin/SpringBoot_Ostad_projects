95fa4d0ac2491182663395863ef856d22fa929be
SHERAJUS SALEHIN <66803588+Sherajus-Salehin@users.noreply.github.com>
2026-04-14 22:52:23 +0600

    rechecks

---
+++ Assignment18/src/main/java/com/salehin/Assignment18/Assignment18Application.java
@@ -0,0 +1,13 @@
+package com.salehin.Assignment18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment18Application.class, args);
+	}
+
+}
---
+++ Assignment18/src/test/java/com/salehin/Assignment18/Assignment18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salehin.Assignment18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
